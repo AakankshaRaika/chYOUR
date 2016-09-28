2b62595d2c7bed900928d1cd366d39f96c8878af
yosephha <yosephha@buffalo.edu>
2016-09-28 17:27:14 -0400

    skeleton code

---
+++ chYOUR/app/src/main/java/com/chyour/Add_Phone_Calendar.java
@@ -0,0 +1,11 @@
+package Features;
+/*
+ * Skeleton code author : @aakanksh @yosephh 
+ * Not assigned yet. (as off sprint 2) 
+ */
+
+public class Add_Phone_Calendar {
+	
+	//We will ask for permission to access the built in phone calender of the user. 
+
+}
---
+++ chYOUR/app/src/main/java/com/chyour/Add_Range.java
@@ -0,0 +1,29 @@
+package Maping;
+
+/*
+ * Skeleton code author : @aakanksh @yosephh 
+ * Task Owners sprint  2: Dom and Vivan. 
+ */
+
+public class Add_Range {
+
+	public Add_Range(){
+		//constructor
+	}
+	
+	public void set_unit(){
+		//chose between KM and Miles.
+	}
+	
+	public void get_unit(){
+		//gets the unit selected
+	}
+	
+	public void set_Range(){
+		//for the selected unit set the range from 1-10 
+	}
+	
+	public void get_Range(){
+	    //get (return ) the range	
+	}
+}
