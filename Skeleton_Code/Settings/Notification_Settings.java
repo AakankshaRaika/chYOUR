b4f7fa2d59b3afe0bf3a5b9198f707d52ca2b5da
AakankshaRaika <aakanksh@buffalo.edu>
2016-09-28 16:43:29 -0400

    Adding skeleton code for Team A. It lets you which tasks and classes to work on. Please look at everything once.

---
+++ Skeleton_Code/Settings/Notification_Settings.java
@@ -0,0 +1,27 @@
+package Settings;
+/*
+ * Skeleton code author : @aakanksh @yosephh 
+ * Not assigned yet. (as off sprint 2) 
+ * the notifications of our app will run independent of your phone settings. No phone settings will affect your chYOUR :D
+ */
+public class Notification_Settings {
+ public Notification_Settings(){
+	 //constructor 
+ }
+ public void snooze(){
+	 //this will give the user an other to snooze the reminder for a set amount of time. 
+ }
+ public void silent(){
+	 //will still give you text based notifications but will not make any sounds. 
+ }
+ public void vibrate(){
+	 // will vibrate your phone. 
+ }
+ public void do_not_disturb(){
+	 //will not notify you what so ever. 
+ }
+	
+ public void voice(){
+	 //you can chose to have our voice notification on or off. 
+ }
+}
