bf67eef71dab055b8f31bf08d686f17cf6fb47bd
yosephha <yosephha@buffalo.edu>
2016-10-03 00:32:22 -0400

    skeleton code

---
+++ chYOUR/app/src/main/java/com/chyour/Add_Google_Calendar.java
@@ -0,0 +1,12 @@
+package Features;
+
+/*
+ * Skeleton code author : @aakanksh @yosephh 
+ * Not assigned yet. (as off sprint 2) 
+ */
+
+public class Add_Google_Calendar {
+	
+//We will let the user log into their google account and pull the tasks in their google calendar. 
+	
+}
