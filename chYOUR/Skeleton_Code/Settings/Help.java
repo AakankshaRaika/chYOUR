2ec91415407d30999f26d4b6cba3899fe8b7f1c1
brylee123 <bclee@buffalo.edu>
2016-10-05 20:41:20 -0400

    Help page now prints FAQ then User Guide. // Barry and Aakanksha

--- chYOUR/Skeleton_Code/Settings/Help.java
+++ chYOUR/Skeleton_Code/Settings/Help.java
@@ -1,15 +1,69 @@
 package Settings;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /*
  * Skeleton code author : @aakanksh @yosephh 
- * Task Owners sprint 2 : Aakanskha Barry and Yosephh
+ * Task Owners sprint 2 : Aakanskha and Barry
  */
 public class Help {
 
 	/*
-	 * Make FAQ page 
-	 * how to use app page 
-	 * what are default locations page 
-	 * support page 
-	 * contact us page. 
+	 * Make FAQ page // Done
+	 * how to use app page  // Done
+	 * what are default locations page // Done
+	 * contact us page
 	 */
+	BufferedReader br = null;
+
+	try {
+
+		String sCurrentLine;
+
+		br = new BufferedReader(new FileReader("FAQ.txt"));
+
+		while ((sCurrentLine = br.readLine()) != null) {
+			System.out.println(sCurrentLine);
+		}
+
+	} catch (IOException e) {
+		e.printStackTrace();
+	} finally {
+		try {
+			if (br != null)br.close();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	
+	BufferedReader br2 = null;
+
+	try {
+
+		String sCurrentLine;
+
+		br2 = new BufferedReader(new FileReader("UserGuide.txt"));
+
+		while ((sCurrentLine = br2.readLine()) != null) {
+			System.out.println(sCurrentLine);
+		}
+
+	} catch (IOException e) {
+		e.printStackTrace();
+	} finally {
+		try {
+			if (br2 != null)br2.close();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	
+	
+	
+	
+	
 }
