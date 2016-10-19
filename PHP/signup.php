321dce16b9a86051c9162e031835a11f458d9c0d
Alec R Tuohy <alectuoh@timberlake.cse.buffalo.edu>
2016-10-18 20:13:25 -0400

    made sign up easier for db

--- PHP/signup.php
+++ PHP/signup.php
@@ -5,13 +5,11 @@ if(mysqli_connect_errno($con))
 	echo '{"query_result":"ERROR"}';
 }
 
-$userName = $_GET['username'];
-$firstName = $_GET['firstname'];
-$lastName = $_GET['lastname'];
+$fullName = $_GET['fullname'];
 $email = $_GET['email'];
 $passWord = $_GET['password'];
 
-$result = mysqli_query($con, "INSERT INTO usertbl (username, firstname, lastname, email, password) VALUES ('$userName', '$firstName', '$lastName', '$email', '$passWord')");
+$result = mysqli_query($con, "INSERT INTO usertbl (fullname, email, password) VALUES ('$fullName, '$email', '$passWord')");
 
 if($result == true){
 	echo '{"query_result":"SUCCESS"}';
