31e8754724e78da21b0044729adcb63f1bc5e0d5
yosephha <yosephha@buffalo.edu>
2016-10-19 11:25:47 -0400

    Is a class that contains login and signup url

---
+++ chYOUR/app/src/main/java/com/chyour/AppConfig.java
@@ -0,0 +1,13 @@
+package com.chyour;
+
+/**
+ * Created by yoseph on 10/19/16.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://128.205.44.23/chyour/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://128.205.44.23/chyour/signup.php";
+}
