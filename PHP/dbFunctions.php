eaa409ec70f8384a101908fbe5dae165bbceb721
Jason Hatfield <jhatfield56@gmail.com>
2016-11-03 17:52:11 -0400

    Adding minutes, server, and php from development branch before replacing it

---
+++ PHP/dbFunctions.php
@@ -0,0 +1,84 @@
+<?php
+
+class dbFunctions {
+ 
+    	private $conn;
+
+	// Constructor
+
+    	function __construct() {
+		$username = "phpuser";
+   		$password = "chyour2016";
+    		$database = "chyourDB";
+		$server = "127.0.0.1";
+
+		$this->conn = mysqli_connect($server,$username,$password);
+		$db_found = mysqli_select_db($this->conn,$database);
+    	}
+    	
+	// Testing code
+
+	/*if($db_found){
+    		print "Database Found!!";
+    		mysqli_close($conn);
+    	}else{
+        	print "Database not found!!!";
+    	}
+    	
+    	require_once 'dbConnect.php';
+    	// connecting to database
+    	$db = new dbConnect();
+    	$this->conn = $db->connect();
+    	*/
+    
+    	// Destructor
+
+    	function __destruct() {
+    	
+	}
+
+	// Attempt to store user in database
+    
+	public function storeUser($fullname, $email, $password) {
+        	//$uuid = uniqid('', true);
+ 
+	        $stmt = $this->conn->prepare("INSERT INTO usersTBL(fullname, email, password) VALUES(?, ?, ?)");
+        	$stmt->bind_param('sss',/* $uuid,*/ $fullname, $email, $password);
+        	$result = $stmt->execute();
+        	$stmt->close();
+		 
+	        // Check for successful store
+
+        	if ($result) {
+            		$stmt = $this->conn->prepare("SELECT * FROM usersTBL WHERE email = ?");
+            		$stmt->bind_param('s', $email);
+            		$stmt->execute();
+            		$user = $stmt->get_result()->fetch_assoc();
+            		$stmt->close();
+			return $user;
+        	} else {
+            		return false;
+        	}
+	}
+     
+	// Check if user exists
+     
+	public function userExists($email) {
+	        $stmt = $this->conn->prepare("SELECT email from usersTBL WHERE email = ?");
+        	$stmt->bind_param("s", $email);
+	        $stmt->execute();
+	        $stmt->store_result();
+ 
+        	if ($stmt->num_rows > 0) {
+            		// user exists 
+            		$stmt->close();
+            		return true;
+        	} else {
+         	   	// user does not exist
+            		$stmt->close();
+            		return false;
+        	}
+    	}
+}
+
+?>
--- PHP/login.php
+++ PHP/login.php
@@ -1,24 +1,23 @@
 <?php
 if($_POST)
 {
-$host="localhost";
-$user="id11983_root";
+$host="127.0.0.1";
+$user="phpuser";
 $pass="chyour2016";
-$db="id11983_chyourdb";
+$db="chyourdb";
 	$username=$_POST['username'};
-$password=$_POST['password'];
-$conn=mysqli_connect($host,$user,$pass,$db);
-	$query="SELECT * from userstbl where username='$username' and password='$password'";
+	$password=$_POST['password'];
+	$conn=mysqli_connect($host,$user,$pass);
+	$query="SELECT * from userstbl where email='$email' and password='$password'";
 	$result=mysqli_query($conn, $query);
+
 	if(mysqli_num_rows($result)==1)
 	{
 		session_start();
-		$_SESSION['id11983_chyourdb']='true';
+		$_SESSION['chyourdb']='true';
 		header('location:index.php');
 	}
 	else {echo 'wrong username or password';}
-
-
 }
 ?>
 
